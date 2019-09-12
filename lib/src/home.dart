162070c6103f5dfeaeb0c69ac7f316e25be691c6
Rodolfo Sanches <rodolfo@bubb.com.br>
2019-09-12 12:56:20 -0300

    Feito request para Api

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   @override
@@ -6,6 +8,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  String _search = "marcelo";
+  Future<Map> _getUsers() async {
+    http.Response response;
+    
+    response = await http.get("https://api.github.com/search/users?q=${_search}&per_page=10");
+
+    return json.decode(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,39 +42,76 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 4,
-                  crossAxisSpacing: 4,
-                  childAspectRatio: 0.65,
-                ),
-              itemCount: 4,
-              padding: EdgeInsets.all(4.0),
-              itemBuilder: (context, index) {
-                  return InkWell(
-                    child: Card(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          AspectRatio(
-                            aspectRatio: 0.8,
-                            child: Image.network(
-                              "https://uploads.metropoles.com/wp-content/uploads/2019/03/27172427/WhatsApp-Image-2019-03-27-at-16.54.41.jpeg",
-                              fit: BoxFit.cover
-                            )
+            child: FutureBuilder<Map>(
+              future: _getUsers(),
+              builder: (context, snapshot){
+                switch (snapshot.connectionState){
+                  case ConnectionState.waiting:
 
-                          )
-                        ],
+                  case ConnectionState.none:
+                    return Container(
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+
                       ),
-                    ),
-                  );
+
+                    );
+
+                  default:
+                    if (snapshot.hasError && !snapshot.hasData) {
+                      return Container();
+                    } else {
+                      return _createGitTable(context, snapshot);
+                    }
+                }
               },
-            ),
+            )
           )
         ],
       ),
+    );
+  }
+
+  Widget _createGitTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        childAspectRatio: 0.65,
+      ),
+      itemCount: snapshot.data["items"].length,
+      padding: EdgeInsets.all(4.0),
+      itemBuilder: (context, index) {
+        return InkWell(
+          child: Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                        snapshot.data["items"][index]["avatar_url"],
+                        fit: BoxFit.cover
+                    )
+                ),
+                Expanded(
+                    child: Center(
+                      child: Text(
+                          snapshot.data["items"][index]["login"],
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18
+                          )
+                      ),
+                    )
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
